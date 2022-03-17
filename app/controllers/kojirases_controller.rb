22af2de1f02bcfd06e07212d6e16dfcf00ef0e86
saki-okamura <ykhmsk@gmail.com>
2022-03-17 15:31:36 +0900

    showファイルの設定

--- app/controllers/kojirases_controller.rb
+++ app/controllers/kojirases_controller.rb
@@ -5,5 +5,25 @@ class KojirasesController < ApplicationController
     @kojirase = Kojirase.new
   end
 
+  def show
+    @kojirase = Kojirase.find(params[:id])
+  end
+
+  def create
+    @kojirase = Kojirase.new(kojirase_params)
+    @kojirase.question = params[:kojirase][:question].join("")
+    if @kojirase.save
+      flash[:success] = '診断が完了しました'
+      redirect_to kojirase_path(@kojirase)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def kojirase_params
+    params.require(:kojirase).permit(question: [])
+  end
 
 end
